a83418ef93a915342b3f862e20c46d94dd838bd0
Cuong98 <nmcuong3198@gmail.com>
2026-08-30 17:24:50 +0700

    update footer

--- src/app/layouts/MainLayout.jsx
+++ src/app/layouts/MainLayout.jsx
@@ -1,9 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Header from "@/shared/components/layout/Header/Header";
 import Footer from "@/shared/components/layout/Footer/Footer";
 
 export default function MainLayout() {
+    const location = useLocation();
+    const isUnifiedFooterPage = location.pathname === "/" || location.pathname === "/download";
+
     return (
         <>
             <Header />
@@ -12,7 +15,7 @@ export default function MainLayout() {
                 <Outlet />
             </main>
 
-            <Footer />
+            {!isUnifiedFooterPage && <Footer />}
         </>
     );
 }
