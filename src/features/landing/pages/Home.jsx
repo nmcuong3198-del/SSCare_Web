5ba7bcb6e1ddcaa912a651fb61aa8a6cdd487d3a
Cuong98 <nmcuong3198@gmail.com>
2026-08-30 17:34:51 +0700

    update footer

--- src/features/landing/pages/Home.jsx
+++ src/features/landing/pages/Home.jsx
@@ -4,6 +4,7 @@ import HeroSection from "@/features/landing/components/Hero/Hero";
 import MissionSection from "@/features/landing/components/Mission/Mission";
 import TestimonialSection from "@/features/landing/components/Testimonial/Testimonials";
 import DownloadSection from "@/features/landing/components/Download/Download";
+import Footer from "@/shared/components/layout/Footer/Footer";
 
 export default function Home() {
     return (
@@ -12,8 +13,9 @@ export default function Home() {
             <MissionSection />
             <TestimonialSection />
 
-            <div className="download-section-background">
+            <div className="download-footer-unified">
                 <DownloadSection />
+                <Footer />
             </div>
         </>
     );
